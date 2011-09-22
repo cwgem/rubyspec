3411096e6d8d70d2683c2b79460a9d3f11652897
Chris White <cwprogram@live.com>
2011-09-21 22:27:31 -0700

    Added base description for what tsort achieves.

--- library/tsort/tsort_spec.rb
+++ library/tsort/tsort_spec.rb
@@ -2,6 +2,11 @@ require File.expand_path('../../../spec_helper', __FILE__)
 require File.expand_path('../fixtures/classes.rb', __FILE__)
 
 describe "Tsort#tsort" do
+  it "returns a topologically sorted array of nodes" do
+    h = TSortHash[{1=>[2, 3], 2=>[3], 3=>[]}]
+    h.tsort.should == [3,2,1]
+  end
+
   it "raises a TSort::Cyclic exception if there is a cycle" do
     h = TSortHash[{1=>[2], 2=>[3, 4], 3=>[2], 4=>[]}]
     lambda { h.tsort }.should raise_error(TSort::Cyclic)
